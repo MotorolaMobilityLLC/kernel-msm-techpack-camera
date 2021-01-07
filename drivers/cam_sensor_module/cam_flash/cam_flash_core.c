11104dfdd84af2944c67b35a417c173f09a034b8
Jeevitha Jayanth <fxq364@motorola.com>
2021-01-07 11:57:28 +0530

    Invalidate flash request after its processed
    Invalidate flash request after flash settings are applied so that it doesnt
    get processed again during FLUSH operation
    
    Change-Id: I8d143602927c98aa37babf700b4de120cf278d49
    Reviewed-on: https://gerrit.mot.com/1845490
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key

--- drivers/cam_sensor_module/cam_flash/cam_flash_core.c
+++ drivers/cam_sensor_module/cam_flash/cam_flash_core.c
@@ -1506,6 +1506,7 @@ static int cam_flash_i2c_delete_req(struct cam_flash_ctrl *fctrl,
 {
 	int i = 0, rc = 0;
 	uint64_t top = 0, del_req_id = 0;
+	int frame_offset = 0;
 
 	if (req_id != 0) {
 		for (i = 0; i < MAX_PER_FRAME_ARRAY; i++) {
@@ -1534,6 +1535,10 @@ static int cam_flash_i2c_delete_req(struct cam_flash_ctrl *fctrl,
 		CAM_DBG(CAM_FLASH, "top: %llu, del_req_id:%llu",
 			top, del_req_id);
 	}
+
+	/* delete/invalidate the request */
+	frame_offset = del_req_id % MAX_PER_FRAME_ARRAY;
+	fctrl->i2c_data.per_frame[frame_offset].is_settings_valid = false;
 
 	cam_flash_i2c_flush_nrt(fctrl);
 
