603866cadb8ac35bc769362ff2cec8fea21bcc04
Dawei Wang <wangdw10@motorola.com>
2021-03-02 19:31:56 -0600

    Revert "(CR): msm: camera: Early apply flash SOF settings"
    QC confirm "Dont subscribe for eof trigger in flash subdevice" is the
    necessary change for this issue, so revert this change.
    
    This reverts commit 8d9c3eb01d360773564c001c8c0cc25c93b7006c.
    
    Change-Id: Idebacba38c8ba4f31e934d8010556e2df7cdc226
    Reviewed-on: https://gerrit.mot.com/1891033
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Dawei Wang <wangdw10@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Reviewed-by: Yongqin Wan <wanyq1@motorola.com>
    Reviewed-by: Zilong Zhang <zhangzl7@mt.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/cam_isp/isp_hw_mgr/isp_hw/tfe_hw/cam_tfe_core.c
+++ drivers/cam_isp/isp_hw_mgr/isp_hw/tfe_hw/cam_tfe_core.c
@@ -2087,15 +2087,10 @@ static int cam_tfe_camif_resource_start(
 
 	/* Epoch config */
 	epoch0_irq_mask = (((rsrc_data->last_line + rsrc_data->vbi_value) -
-			rsrc_data->first_line) / 5);
+			rsrc_data->first_line) / 2);
 	if (epoch0_irq_mask > (rsrc_data->last_line - rsrc_data->first_line))
 		epoch0_irq_mask = (rsrc_data->last_line -
 					rsrc_data->first_line);
-
-	CAM_DBG(CAM_ISP,
-		"TFE:%d first_line: %u last_line: %u vbi: %u epoch0_irq_mask: 0x%x",
-		core_info->core_index, rsrc_data->first_line,
-		rsrc_data->last_line, rsrc_data->vbi_value, epoch0_irq_mask);
 
 	epoch1_irq_mask = rsrc_data->reg_data->epoch_line_cfg &
 			0xFFFF;
